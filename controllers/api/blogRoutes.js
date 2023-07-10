03a136b1321f42966510d76d0cb7d09a9e6b84d0
Andrew <ATHur1104@gmail.com>
2023-07-09 21:29:56 -0500

    Updating BlogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -11,13 +11,57 @@ router.post('/', withAuth, async (req, res) => {
     });
 
     req.status(200).json(dbBlogData);
-    
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+// DELETE blog post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const dbBlogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      }
+    });
 
+    if (!dbBlogData) {
+      res.status(404).json({ message: 'No Blog Post found with this ID' });
+      return;
+    };
+
+    res.status(200).json(dbBlogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+// UPDATE blog post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const dbBlogData = await Blog.update({
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+    );
+
+    if (!dbBlogData) {
+      res.status(404).json({ message: 'No Blog Post found with this ID' });
+      return;
+    };
+
+    res.status(200).json(dbBlogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
