d079b2c7ffeb6e9d81b7cd6bedf0392f45d37402
Andrew <ATHur1104@gmail.com>
2023-07-13 14:59:35 -0500

    updating to fix heroku issues

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const hbs = exphbs.create({ helpers});
 
 // Configure and link a session object with the sequelize store
 const sess = {
-  secret: process.env.SECRET,
+  secret: "SECRET secret Code",
   cookie: {},
   resave: false,
   saveUninitialized: true,
